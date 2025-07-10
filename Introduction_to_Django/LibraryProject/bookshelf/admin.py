22b279ddf9ad4a001622af578576fc83313db643
KSENOU <kalifasenou10@gmail.com>
2025-07-10 18:19:32 +0000

    correct du file admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,16 @@
-from django.contrib import admin
-from .models import Book 
+from django.contrib import admin 
+from .models import Book      
 
-# Da custom ModelAdmin class 
-class BookAdmin(admin.ModelAdmin): 
+class BookAdmin(admin.ModelAdmin):
+
+    # list_display: Controls which fields are displayed as columns in the change list page.
     list_display = ('title', 'author', 'publication_year')
 
-    # This adds filters to the right sidebar in the admin list view
-    list_filter = ('publication_year', 'author') 
+    # list_filter: Adds a right-sidebar filter box to the change list page,
+    list_filter = ('publication_year', 'author')
 
-    # This adds a search box to the top of the admin list view, 
-    search_fields = ('title', 'author') 
+    # search_fields: Adds a search box to the top of the change list page,
+    search_fields = ('title', 'author')
+
+
+admin.site.register(Book, BookAdmin)
