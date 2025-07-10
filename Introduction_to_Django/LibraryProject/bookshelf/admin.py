5b737f5e7889a9316dd6b131dea8e97854f610e1
KSENOU <kalifasenou10@gmail.com>
2025-07-10 17:47:55 +0000

    Configured Book model admin with list display, filters, and search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book 
 
-# Register your models here.
+# Da custom ModelAdmin class 
+class BookAdmin(admin.ModelAdmin): 
+    list_display = ('title', 'author', 'publication_year')
+
+    # This adds filters to the right sidebar in the admin list view
+    list_filter = ('publication_year', 'author') 
+
+    # This adds a search box to the top of the admin list view, 
+    search_fields = ('title', 'author') 
