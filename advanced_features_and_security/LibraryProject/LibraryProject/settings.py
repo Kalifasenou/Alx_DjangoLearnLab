7e97490016c3dce7bdf81b05bfd8b3f9d5f7a648
KSENOU <kalifasenou10@gmail.com>
2025-07-21 22:41:55 +0000

    Tâche 2 : Mise en œuvre des meilleures pratiques de sécurité

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -124,3 +124,19 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+DEBUG = False
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+
+MIDDLEWARE = [
+    # ...
+    'csp.middleware.CSPMiddleware',
+]
+
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'", "https://trusted.cdn.com")
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
+from advanced_features_and_security.LibraryProject.bookshelf.models import Book
+
 
 # Create your views here.
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    # Logique de création
+# Logique de création
 
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, pk):
-    # Logique d'édition 
+# Logique d'édition ghttqhth hsryzb tyznzn 
+    return "eeeeeeee"
 
+
+def search_books(request):
+    query = request.GET.get('q', '')
+    # Méthode sécurisée :
+    books = Book.objects.filter(title__icontains=query)
