42537a7c2e9cf15f2f7fc011c232f44a0e06adcb
KSENOU <kalifasenou10@gmail.com>
2025-07-30 13:18:07 +0000

    correction erreur tache 0 auto correcteur

--- advanced_features_and_security/LibraryProject/users/models.py
+++ advanced_features_and_security/LibraryProject/users/models.py
@@ -1,38 +1,25 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils import timezone
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
+class CustomUserManager(UserManager):
+    # Hérite de UserManager pour conserver create_user/create_superuser standard
+    pass
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField('email address', unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    username = models.CharField(max_length=45, blank=False)
-    date_of_birth = models.DateField(null=True, blank=True)  # Champ ajouté
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)  # Champ ajouté
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+class CustomUser(AbstractUser):
+    """
+    Hérite d'AbstractUser pour inclure tous les champs par défaut
+    (username, first_name, last_name, email, password, etc.)
+    et y ajouter date_of_birth et profile_photo.
+    """
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
+    # Remplacer le manager par défaut
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return self.email or self.username
+
 
 
