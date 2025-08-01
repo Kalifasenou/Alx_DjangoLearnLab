0ec2448b78f71d19762e2607bcfefd718db5ce2b
KSENOU <kalifasenou10@gmail.com>
2025-08-01 13:31:25 +0000

    tester avec le token

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,13 +3,15 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 # Create your views here.
 
+
 #afficher tous les livres
 class BookList(ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
