5a4e7534e2b45d854444dc26752ca35786d6f2f3
KSENOU <kalifasenou10@gmail.com>
2025-08-17 12:26:35 +0000

    Checks for the updated urls of the new views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,11 +14,17 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 
     # posts CRUD
-    path('', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    #path('', views.PostListView.as_view(), name='post-list'),
+    #path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    #path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    #path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-edit'),
+    #path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
     # comments
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-edit'),
