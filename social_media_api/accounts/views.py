6053929afd6a0b00d60f55d975cf2c0a135294c4
KSENOU <kalifasenou10@gmail.com>
2025-08-21 22:34:31 +0000

    ajout des views de creations de compte, de connexion et de profils dans le file views.py de accounts

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from .models import User
 
-# Create your views here.
+# Mes views.
+
+#view de creation de compte
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data["username"]) # creation de compte
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+
+
+#view de connexion
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
+
+
+#view de profils user
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    
+    def get_object(self):
+        return self.request.user
