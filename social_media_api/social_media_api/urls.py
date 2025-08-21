865c86f8cc18398223454ba2098d6cfc5438b9f8
KSENOU <kalifasenou10@gmail.com>
2025-08-21 22:43:00 +0000

    mise à jour de mon urls.py principal pour acceder aux differents views de mon app

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+# routes principales d'access aux diffs contenus de mon app
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    
+    # /inscription, /login, /profile
+    path("accounts/", include("accounts.urls")), 
+
+    # gestions posts & comments
+    path("api/", include("posts.urls")), 
+    
+    # gestions notifications 
+    path("api/", include("notifications.urls")), 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
