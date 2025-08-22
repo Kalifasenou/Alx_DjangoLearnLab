184b1bf887a7758cfa4452c1187967d3afe71616
KSENOU <kalifasenou10@gmail.com>
2025-08-22 20:44:47 +0000

    file view pour gerer les diff vu de creation de post et commentaire

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
 
 # Create your views here.
+
+#view pour la creation de poste
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+#vue pour afficher info d'un poste
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+#vu de creation de commentaire
+class CommentCreateView(generics.CreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs["post_id"]
+        serializer.save(user=self.request.user, post_id=post_id)
