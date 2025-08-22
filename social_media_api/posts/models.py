e0f5e8476204c81131b5bb881b50860f5a5ae1aa
KSENOU <kalifasenou10@gmail.com>
2025-08-22 00:12:04 +0000

    ajout des models postes et commentaires au models file de posts

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+# Mes models pour les publications et commentaires
+
+#Import paramettre d'authentification
+User = settings.AUTH_USER_MODEL
+
+#creation d'un post
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+#creation de commentaire
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post_id}"
